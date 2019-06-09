dd67d40587b918d07ca0a72ac55ccedad8fd597e
annaCPT <51199923+annaCPT@users.noreply.github.com>
2019-06-08 22:13:40 -0400

    Update trivia.js

--- trivia.js
+++ trivia.js
@@ -10,13 +10,13 @@ if (ans===1){
     y = document.getElementById("qans").value
     if(yy==="C"){
         document.getElementById("question").value="You are right"
-	     document.write("<img src='smile.gif' width="200" height="400">")
+	     document.write("<img src='smile.gif'>")
         score++
         
     }
     else {
         document.getElementById("question").value="You are wrong"
-	     document.write("<img src='sad.gif' width="200" height="400">")
+	     document.write("<img src='sad.gif'>")
         
     }
     document.view.qscore.value=score
